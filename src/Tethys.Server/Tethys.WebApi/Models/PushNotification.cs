bd810c22e8995ffa4d6553cb09dd9dcd2de38308
Saturn Technologies <info@saturn72.com>
2018-07-03 15:06:14 +0300

    Move to vscode

--- src/Tethys.Server/Tethys.WebApi/Models/PushNotification.cs
+++ src/Tethys.Server/Tethys.WebApi/Models/PushNotification.cs
@@ -1,9 +1,20 @@
-﻿namespace Tethys.WebApi.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tethys.WebApi.Models
 {
     public class PushNotification
     {
-        public string Key { get; set; }
+        [Required] public string Key { get; set; }
         public int Delay { get; set; }
-        public string Body { get; set; }
+        [Required] public string Body { get; set; }
+
+        public int NotifyTimes { get; set; }
+        public int NotificationCounter { get; set; }
+
+        public long Id { get; set; }
+
+        public bool WasNotified { get; set; }
+        public DateTime? NotifiedOnUtc { get; set; }
     }
 }
