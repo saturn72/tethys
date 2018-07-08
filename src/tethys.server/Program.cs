8e8da09a63e906ac599ac5f3f3d541ac0a4e7f46
Saturn Technologies <info@saturn72.com>
2018-07-08 12:25:38 +0300

    add serilog

--- src/tethys.server/Program.cs
+++ src/tethys.server/Program.cs
@@ -7,39 +7,63 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Tethys.Server
 {
     public class Program
     {
-        private static IConfiguration _configuration;
+        public static IConfiguration Configuration { get; set; }
 
         public static int Main(string[] args)
         {
-            var configFilePath = args.Length >= 2 ? args[1] : "appsettings.json";
-            var configDirectory = Path.GetDirectoryName(configFilePath);
-            if (configDirectory.Length == 0)
-                configDirectory = Directory.GetCurrentDirectory();
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
 
-            _configuration = new ConfigurationBuilder()
-                .SetBasePath(configDirectory)
-                .AddJsonFile(configFilePath)
-                .AddCommandLine(args)
-                .Build();
+            try
+            {
+                Log.Information("Starting web host");
 
-            CreateWebHostBuilder(args).Build().Run();
-            return 0;
+                var configFilePath = args.Length >= 2 ? args[1] : "appsettings.json";
+                var configDirectory = Path.GetDirectoryName(configFilePath);
+                if (configDirectory.Length == 0)
+                    configDirectory = Directory.GetCurrentDirectory();
+
+                Configuration = new ConfigurationBuilder()
+                    .SetBasePath(configDirectory)
+                    .AddJsonFile(configFilePath)
+                    .AddCommandLine(args)
+                    .Build();
+
+                CreateWebHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            var config = TethysConfig.FromConfiguration(_configuration);
+            var config = TethysConfig.FromConfiguration(Configuration);
             var urls = config.HttpPorts.Select(hp => "http://localhost:" + hp).ToArray();
 
             return WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(_configuration)
+                .UseConfiguration(Configuration)
                 .UseStartup<Startup>()
+                .UseSerilog()
                 .UseUrls(urls);
         }
     }
